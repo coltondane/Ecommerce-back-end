29e9378f4a96d229b00df45d3a6ad55be09f2bd0
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-11 15:57:24 -0700

    product routes finally done

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,53 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [Category,
+        {
+          model: Tag,
+          through: ProductTag
+        }
+      ]
+    });
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+    return;
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: { id: req.params.id },
+      include: [Category,
+        {
+          model: Tag,
+          through: ProductTag
+        }
+      ]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No Product Data found with that id' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -92,8 +126,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No Product Data found with that id' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+    console.log(error);
+  }
 });
 
 module.exports = router;
